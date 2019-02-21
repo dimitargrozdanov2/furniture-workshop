098587a4be492429b587a50f4397cac68f2764b6
grozdeto <gros2@abv.bg>
2019-02-21 12:09:14 +0200

    Installed Autofac

---
+++ Skeleton/FurnitureManufacturer/FurnitureProgram.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using FurnitureManufacturer.Engine;
+using FurnitureManufacturer.Interfaces.Engine;
+using System.Reflection;
+
+namespace FurnitureManufacturer
+{
+    public class FurnitureProgram
+    {
+        public static void Main()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsImplementedInterfaces();
+            //Add singletons(single instances)
+
+            // Add commands
+
+            var container = builder.Build();
+
+            var engine = container.Resolve<IFurnitureManufacturerEngine>();
+            engine.Start(); 
+            FurnitureManufacturerEngine.Instance.Start();
+        }
+    }
+}
